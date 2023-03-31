87f5871d44485888c2c7c9cbe1af16e2fdd6b687
Pavel Mingaleev <pav.mingaleev@yandex.ru>
2023-03-31 16:07:38 +0500

    Added task69

--- task67/Program.cs
+++ task67/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. 
+// 453 -> 12
+// 45 -> 9
+
+
+Console.Clear(); 
+int n = GetNumberFromUser($"Введите число ", "Ошибка ввода!"); 
+int result = GetResult(n); 
+Console.Write($"{n} -> {result}"); 
+
+
+int GetNumberFromUser(string message, string errorMessage) 
+{
+ while (true) 
+    {
+ Console.Write(message); 
+ bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber); 
+ if (isCorrect) 
+ return userNumber; 
+ Console.WriteLine(errorMessage); 
+    }
+}
+
+int GetResult(int n) 
+{
+ if (n == 0) return 0; 
+    else
+    {
+        
+        GetResult(n - 1);
+        return n%10 + GetResult(n/10);
+    }
+}
