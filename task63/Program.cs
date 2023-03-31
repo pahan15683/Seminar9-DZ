6e1c6e219950fbb16a8568c9e623a304d853f99e
Pavel Mingaleev <pav.mingaleev@yandex.ru>
2023-03-31 12:06:22 +0500

    Added task65

--- task63/Program.cs
+++ task63/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.WriteLine("введите натуральное число");
+int number = int.Parse(Console.ReadLine());
+NaturalNumbers(number);
+void NaturalNumbers(int num)
+{
+    if(num == 0) return;
+   
+    Console.Write($"{num} ");
+     NaturalNumbers(num - 1);
+}
