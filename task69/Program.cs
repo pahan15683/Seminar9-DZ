0b6b08118bf3a7b4ceb900875d3fcd4a38bd56ef
Pavel Mingaleev <pav.mingaleev@yandex.ru>
2023-03-31 16:23:24 +0500

    Added task69

--- task69/Program.cs
+++ task69/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии. 
+// A = 3; B = 5 -> 243 (3⁵) 
+// A = 2; B = 3 -> 8 
+
+Console.Clear(); 
+int a = GetNumberFromUser($"Введите число ", "Ошибка ввода!"); 
+int b = GetNumberFromUser($"Введите число ", "Ошибка ввода!"); 
+int result = GetResult(a, b); 
+Console.Write($"A = {a}; B = {b} -> {result}"); 
+
+
+int GetNumberFromUser(string message, string errorMessage) 
+{
+ while (true) 
+    {
+ Console.Write(message); 
+ bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber); 
+ if (isCorrect) 
+ return userNumber; 
+ Console.WriteLine(errorMessage); 
+    }
+}
+
+int GetResult(int a, int b) 
+{
+ if (b == 1) return a; 
+    {
+ return a*GetResult(a, b-1); 
+    }
+}
