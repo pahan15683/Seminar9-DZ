f119e15379c4226daf55ed8c76f411fe2cbc4823
Pavel Mingaleev <pav.mingaleev@yandex.ru>
2023-03-31 15:48:33 +0500

    Added task66

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.WriteLine("Введите натуральное число больше 1:");
+int num = int.Parse(Console.ReadLine());
+
+void NumberCounter (int num)
+{
+    if (num < 0) 
+    {
+    Console.Write($"{num} не натуральное число");
+    }
+
+    if (num == 0) 
+    {
+      return;
+    }
+    Console.Write("{0,4}", num);
+    NumberCounter (num - 1);
+}
+NumberCounter(num);
+
