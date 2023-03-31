e4474638e4636c7f059d454c29853b1868096fcb
Pavel Mingaleev <pav.mingaleev@yandex.ru>
2023-03-31 15:13:28 +0500

    Added task04

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно
+// число (N), а на выходе показывает все целые числа 
+// в промежутке от -N до N. 
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = -n;
+
+while (count < n+1)
+{
+    Console.WriteLine(count + " ");
+    count ++;
+}
