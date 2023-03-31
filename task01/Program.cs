74245dc90ebe981c8153334715afea208604be64
Pavel Mingaleev <pav.mingaleev@yandex.ru>
2023-03-31 14:59:47 +0500

    Added task02

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго
+// a=25, b=5 -> да;   a=2, b=10 -> нет;    a=9, b=-3 -> да; // a=-3, b=9 -> нет
+
+ Console.WriteLine("Введите число");
+ float a = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Введите число");
+ float b = Convert.ToInt32(Console.ReadLine());
+ if (a == b * b)
+ {
+    Console.WriteLine("да");
+ }
+ else
+ {
+Console.WriteLine("нет");
+ }
